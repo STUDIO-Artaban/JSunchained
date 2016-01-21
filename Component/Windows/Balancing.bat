45470bece6691d0672b84b9b300ece1ab784f1cf
Pascal Viguié <pascal.viguie@studio-artaban.com>
2016-01-21 11:34:49 +0100

    Create Balancing.bat

---
+++ Component/Windows/Balancing.bat
@@ -0,0 +1,19 @@
+REM # IN x64 DEBUG MODE ONLY
+
+SET YourSiteAppPath="D:\Dev\YourSiteApp\YourSiteApp\bin\x64\Debug\AppX"
+
+REM copy D:\Dev\boost_1_59_0\lib64-msvc-14.0\boost_system-vc140-mt-gd-1_59.dll %YourSiteAppPath%
+REM copy D:\Dev\boost_1_59_0\lib64-msvc-14.0\boost_thread-vc140-mt-gd-1_59.dll %YourSiteAppPath%
+
+REM copy D:\gstreamer\1.0\x86_64\bin\libffi-6.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libglib-2.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libgmodule-2.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libgobject-2.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libgstapp-1.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libgstbase-1.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libgstreamer-1.0-0.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libintl-8.dll %YourSiteAppPath%
+REM copy D:\gstreamer\1.0\x86_64\bin\libwinpthread-1.dll %YourSiteAppPath%
+
+copy D:\Dev\JSunchained\x64\Debug\JSunchained\JSunchained.dll %YourSiteAppPath%
+copy D:\Dev\JSunchained\x64\Debug\Unchained\Unchained.dll %YourSiteAppPath%
